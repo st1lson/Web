2778e882059647675f43d65142a9630dd762cfac
Max <rendon4ik@gmail.com>
2021-10-10 12:42:09 +0300

    Edit config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-	entry: './Lab2/client/src/index.jsx',
+	entry: './Lab2/client/src/index.js',
 	output: {
 		filename: 'bundle.[hash].js',
 		path: path.resolve(__dirname, './Lab2/client/dist'),
