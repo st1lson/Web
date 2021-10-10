80589cb6411557016529373382b16ac1ed3c33c7
Max <rendon4ik@gmail.com>
2021-10-10 21:58:22 +0300

    Add Layout scss

--- Lab2/client/src/Layout/Layout.jsx
+++ Lab2/client/src/Layout/Layout.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Style from './Layout.scss';
+import logo from './logo.png';
 
 const Layout = props => {
 	const { children } = props;
 
 	return (
 		<div>
-			<h1>Mail sender</h1>
+			<header>
+				<p>
+					<a href="https://github.com/st1lson/Web/tree/lab2">
+						<img src={logo} className={Style.Img} alt="logo" width="100" height="100"/>
+					</a>
+				</p>
+				<h1>Mail sender</h1>
+			</header>
 			<main>
 				{children}
 			</main>
