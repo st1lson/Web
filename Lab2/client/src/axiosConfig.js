38d14200ae3a594316342df0d9af4ea231c9feb7
Max <rendon4ik@gmail.com>
2021-10-12 17:36:58 +0300

    Change API base url

--- Lab2/client/src/axiosConfig.js
+++ Lab2/client/src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
 	// Deploy
-	baseURL: 'https://mail-sender-client.azurewebsites.net/api',
+	baseURL: 'https://mail-sender-api.azurewebsites.net/api',
 	// Local
 	//baseURL: 'https://localhost:44396/api', // APS.NET Core Web API URL
 });
