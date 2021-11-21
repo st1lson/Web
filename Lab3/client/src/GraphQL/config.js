494211f81f299b95a1e480703e05e77c70e3bfd8
Max <rendon4ik@gmail.com>
2021-11-21 11:45:57 +0200

    Add environment variables

--- Lab3/client/src/GraphQL/config.js
+++ Lab3/client/src/GraphQL/config.js
@@ -1,6 +1,5 @@
-export const HASURA_ADMIN_SECRET =
-    'R1jLcaDv4iRAEpTV3FWXiYMizryCJGKHBt4LnAUrNRDJDBQ7wRCemsnVFy9AOgs8';
+export const HASURA_ADMIN_SECRET = process.env.X_HASURA_ADMIN_SECRET;
 
-export const URI = 'https://arriving-chamois-37.hasura.app/v1/graphql';
+export const URI = 'https://' + process.env.GRAPHQL_ENDPOINT;
 
-export const URI_WSS = 'wss://arriving-chamois-37.hasura.app/v1/graphql';
+export const URI_WSS = 'wss://' + process.env.GRAPHQL_ENDPOINT;
